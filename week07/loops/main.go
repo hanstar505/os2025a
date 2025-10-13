bac572405e998e6522e32863251c31291ecb151a
hast <hanstar505gmail.com>
2025-10-13 11:56:06 +0900

    main.go v0.1 random dice

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -1,34 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
+	"math/rand"
 )
 
 func main() {
-	//var fmt string = "inha"
-	//var int int = 7
-	//var k in1 = 11
-	//fmt.Println(int)
-
-	r := bufio.NewReader(os.Stdin)
-	i, err := r.ReadString('\n') // ignore error
-	//fmt.Println(err)
-	i = strings.TrimSpace(i)
-	score, err := strconv.ParseFloat(i, 64)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if score >= 60 {
-		fmt.Println("Pass")
-	} else {
-		fmt.Println("fail")
-	}
-	//reoprt the error and exit program
-	fmt.Println(i)
+	rand.Seed(10)
+	dice := rand.Intn(6) + 1 //1 ~ 6
+	fmt.Println(dice)
 }
