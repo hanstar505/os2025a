15ca33641a676b706758a100910d028b60c245c5
hast <hanstar505gmail.com>
2025-11-03 11:57:57 +0900

    week10 package Var v0.2

--- week10/cmd/ch05-ex01/main.go
+++ week10/cmd/ch05-ex01/main.go
@@ -3,10 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var arrauBool [3]bool
+	arrauBool := [3]bool{true, false, true} //배열 리터럴
 	var arrayInt [3]int
 	fmt.Println(arrauBool[1]) //zero value
-	arrayInt[1]++             // arrayInt[1] + 1
-	arrayInt[1]++             // arrayInt[1] + 1
+	arrayInt[1] = 2           // arrayInt[1] + 1
 	fmt.Println(arrayInt[1])  //zero value + 2
 }
