b7db281a4d3d1b45264e214126066e8716c5833c
hast <hanstar505gmail.com>
2025-12-01 11:13:05 +0900

    week14 defer v0.2

---
+++ week14/cmd/ch12-ex01/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	//last in first out
+	defer fmt.Println("1st defer")
+	defer fmt.Println("2nd defer")
+	defer fmt.Println("3rd defer")
+	fmt.Println("main logic")
+}
